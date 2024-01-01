2ea8a72467db419bc7e9e31572d2bff63a7ebfd3
Bolarinwa Musa <sheumusa4real@gmail.com>
2024-01-01 20:28:44 +0100

    Java files

---
+++ calculator/ThreeNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ThreeNumbers;
+public static void main(String [] args){
+Scanner input = new Scanner(System.in);
+System.out.print("Enter first number:");
+int number1 = input.nextInt();
+System.out.print("Enter second number:");
+int number2 = input.nextInt();
+System.out.print("Enter third number:");
+int number3 = input.nextInt();
+ if(number1 > number2, & number3  ) {
+    system.out.print(largest);
+}
+ if(number2 > number1 & number3){
+ System.out.print(largest);
+}
+ if(number3 > number2 & number1){
+ System.out.print(largest);
+  }
+ }
+}
