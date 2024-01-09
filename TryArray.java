beef1a23228a6c8851a4e18e9d325eee1aee590c
Bolarinwa Musa <sheumusa4real@gmail.com>
2024-01-09 21:48:13 +0100

    Exercises

---
+++ TryArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class TryArray {
+public static void main(String[] args) {
+int[] numbers = { 2,3,5,1,4};
+Arrays.sort(numbers);
+System.out.println(numbers.length);
+
+System.out.println(Arrays.toString(numbers));
+ }
+}
