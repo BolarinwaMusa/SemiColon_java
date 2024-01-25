7df9b1c23b8c51db5d444578a407e974351cabb0
Bolarinwa Musa <sheumusa4real@gmail.com>
2024-01-25 21:29:15 +0100

    chapter two

---
+++ Digit.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Accomplishment {
+public static void min(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter an integer:",);
+   }
+ }
