38da1e88a47292db82f5a64ec04f5e9ae762a6ee
Bolarinwa Musa <sheumusa4real@gmail.com>
2024-01-26 03:11:38 +0100

    chapter two

--- Divisible.java
+++ Divisible.java
@@ -4,10 +4,12 @@ public static void main(String[] args){
 Scanner input = new Scanner(System.in);
 System.out.print("Enter a number ");
 int number = input.nextInt();
-if(number % 3 == 0){
-System.out.println("it is divisible by 3");
+  if(number % 3 == 0){
+System.out.println("the number entered is divisible by 3");
 }
-    else 
-System.out.println("not divisible by 3");
+     if(number % 3 == 1) {
+  System.out.println("the number entered is not divisible by 3");
+      }
+    }
   }
- }
+
